646cfe7c0c9766e713444c979aa756e148c934ac
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-09 13:56:37 -0500

    create server and a catch all endpoint to test the api

--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
-// implement your server here
-// require your posts router and connect it here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('*', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome To The Posts Database!',
+  });
+});
