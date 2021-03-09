c5f980baf2ad63ebb19acee5477055204939d233
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-09 14:09:54 -0500

    push first endpoint to get all post tested on postman

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,16 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  Post.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'The posts information could not be retrieved',
+      });
+    });
+});
+
 module.exports = router;
