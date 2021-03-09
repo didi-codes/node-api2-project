a5855b5094fa05305b90c70e59675abb032c1843
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-09 14:01:32 -0500

    import posts-router and create the server.use for the router

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const postRouter = require('./posts/posts-router');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/posts', postRouter);
 
 server.get('*', (req, res) => {
   res.status(200).json({
