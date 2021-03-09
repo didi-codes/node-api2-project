0832d2e6e412ca848a57747c1190bb46c450e2d2
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-09 13:52:25 -0500

    import server, port and server.listen in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// require your server and launch it here
+const server = require('./api/server');
+
+const port = 9000;
+
+server.listen(port, () => {
+  console.log(
+    `Can You Hear It On Port ${port}? Listen the server lives on port ${port}`
+  );
+});
