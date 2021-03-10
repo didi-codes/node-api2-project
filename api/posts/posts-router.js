b3a154114d963bab0bc51260d3a7aaa36fb855a6
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-10 09:42:42 -0500

    put and delete endpoint created test post and put endpoints on postman

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -35,7 +35,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   Post.insert(req.body)
-    .then((post) => {
+    .then(post => {
       if (!post) {
         res.status(400).json({
           message: 'Please provide title and contents for the post',
@@ -44,9 +44,52 @@ router.post('/', (req, res) => {
         res.status(201).json(post);
       }
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).json({
         message: 'There was an error while saving the post to database',
+      });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const changes = req.body;
+  Post.update(req.params.id, changes)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else if (!post) {
+        res.status(404).json({
+          message: 'The post with the specified ID does not exist',
+        });
+      } else {
+        res.status(400).json({
+          message: 'Please provide title and contents for the post',
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The post information could not be modified',
+      });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Post.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: 'Post has been successfully removed',
+        });
+      } else {
+        res.status(404).json({
+          message: 'The post with the ID does not exist',
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The post could not be removed',
       });
     });
 });
