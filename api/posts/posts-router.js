caf8f4ae338cc5f264c9570cf77fbdc58a489936
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-10 10:26:14 -0500

    tested and passed for comment endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -97,4 +97,22 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+router.get('/:id/comments', (req, res) => {
+  Post.findPostComments(req.params.id)
+    .then((posts) => {
+      if (!posts) {
+        res.status(404).json({
+          message: 'The post with the specified ID does not exist',
+        });
+      } else {
+        res.status(200).json(posts);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'The comments information could not be retrieved',
+      });
+    });
+});
+
 module.exports = router;
