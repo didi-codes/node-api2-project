0cbe25864b7f272ecd306133ee826fa50de67b08
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-10 10:13:11 -0500

    modify and correct post and put endpointd to work correctly

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -34,49 +34,52 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  Post.insert(req.body)
-    .then(post => {
-      if (!post) {
-        res.status(400).json({
-          message: 'Please provide title and contents for the post',
-        });
-      } else {
-        res.status(201).json(post);
-      }
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: 'There was an error while saving the post to database',
-      });
+  const newPost = req.body;
+  if (!newPost.title || !newPost.contents) {
+    res.status(400).json({
+      message: 'Please provide title and contents for the post',
     });
+  } else {
+    Post.insert(newPost)
+      .then((post) => {
+        res.status(201).json(post);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: 'There was an error while saving the post to the database',
+        });
+      });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
   const changes = req.body;
-  Post.update(req.params.id, changes)
-    .then(post => {
-      if (post) {
-        res.status(200).json(post);
-      } else if (!post) {
+  try {
+    if (!changes.title || !changes.contents) {
+      res.status(400).json({
+        message: 'Please provide title and contents for the post',
+      });
+    } else {
+      const updatedPost = await Post.update(id, changes);
+      if (!updatedPost) {
         res.status(404).json({
           message: 'The post with the specified ID does not exist',
         });
       } else {
-        res.status(400).json({
-          message: 'Please provide title and contents for the post',
-        });
+        res.status(200).json(updatedPost);
       }
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: 'The post information could not be modified',
-      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'The post information could not be modified',
     });
+  }
 });
 
 router.delete('/:id', (req, res) => {
   Post.remove(req.params.id)
-    .then(count => {
+    .then((count) => {
       if (count > 0) {
         res.status(200).json({
           message: 'Post has been successfully removed',
@@ -87,7 +90,7 @@ router.delete('/:id', (req, res) => {
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({
         message: 'The post could not be removed',
       });
